a0b1b158cbb05efeb72542244f95d3f2f178ae36
rajendherraju <38126617+rajendherraju@users.noreply.github.com>
2024-05-16 17:04:50 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,82 +1,136 @@
-# Creating VPC
-resource "aws_vpc" "eks_vpc" {
-  cidr_block           = var.vpc_cidr
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
+# Creating EKS Cluster
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = var.master_arn
+  version  = var.cluster_version
+
+  vpc_config {
+    subnet_ids = [var.public_subnet_az1_id, var.public_subnet_az2_id]
+  }
 
   tags = {
-    Name = "${var.cluster_name}-vpc"
-    Env  = var.env
-    Type = var.type
+    key   = var.env
+    value = var.type
   }
 }
 
-# Creating Internet Gateway and attach it to VPC
-resource "aws_internet_gateway" "eks_internet_gateway" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  tags = {
-    Name = "${var.cluster_name}-igw"
-    Env  = var.env
-    Type = var.type
-  }
-}
-
-# Using data source to get all Avalablility Zones in region
+# Using Data Source to get all Avalablility Zones in Region
 data "aws_availability_zones" "available_zones" {}
 
-# Creating Public Subnet AZ1
-resource "aws_subnet" "public_subnet_az1" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = var.public_subnet_az1_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[0]
-  map_public_ip_on_launch = true
+# Fetching Ubuntu 20.04 AMI ID
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# Creating kubectl server
+resource "aws_instance" "kubectl-server" {
+  ami                         = data.aws_ami.amazon_linux_2.id
+  instance_type               = var.instance_size
+  associate_public_ip_address = true
+  subnet_id                   = var.public_subnet_az1_id
+  vpc_security_group_ids      = [var.eks_security_group_id]
 
   tags = {
-    Name = "Public Subnet AZ1"
+    Name = "${var.cluster_name}-kubectl"
     Env  = var.env
     Type = var.type
   }
 }
 
-# Creating Public Subnet AZ2
-resource "aws_subnet" "public_subnet_az2" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = var.public_subnet_az2_cidr
-  availability_zone       = data.aws_availability_zones.available_zones.names[1]
-  map_public_ip_on_launch = true
+# Creating Launch Template for Worker Nodes
+resource "aws_launch_template" "worker-node-launch-template" {
+  name = "worker-node-launch-template"
+  block_device_mappings {
+    device_name = "/dev/sdf"
 
-  tags = {
-    Name = "Public Subnet AZ2"
-    Env  = var.env
-    Type = var.type
+    ebs {
+      volume_size = 20
+    }
+  }
+
+  image_id      = var.image_id
+  instance_type = "t2.micro"
+  user_data = base64encode(<<-EOF
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+--==MYBOUNDARY==
+Content-Type: text/x-shellscript; charset="us-ascii"
+#!/bin/bash
+/etc/eks/bootstrap.sh prod-cluster
+--==MYBOUNDARY==--\
+  EOF
+)
+
+
+  vpc_security_group_ids = [var.eks_security_group_id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "Worker-Nodes"
+    }
   }
 }
 
-# Creating Route Table and add Public Route
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
+# Creating Worker Node Group
+resource "aws_eks_node_group" "node-grp" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "Worker-Node-Group"
+  node_role_arn   = var.worker_arn
+  subnet_ids      = [var.public_subnet_az1_id, var.public_subnet_az2_id]
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.eks_internet_gateway.id
+  launch_template {
+    name    = aws_launch_template.worker-node-launch-template.name
+    version = aws_launch_template.worker-node-launch-template.latest_version
   }
 
-  tags = {
-    Name = "Public Route Table"
-    Env  = var.env
-    Type = var.type
+  labels = {
+    env = "Prod"
+  }
+
+  scaling_config {
+    desired_size = var.worker_node_count
+    max_size     = var.worker_node_count
+    min_size     = var.worker_node_count
+  }
+
+  update_config {
+    max_unavailable = 1
   }
 }
 
-# Associating Public Subnet in AZ1 to route table
-resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
-  subnet_id      = aws_subnet.public_subnet_az1.id
-  route_table_id = aws_route_table.public_route_table.id
+locals {
+  eks_addons = {
+    "vpc-cni" = {
+      version           = var.vpc-cni-version
+      resolve_conflicts = "OVERWRITE"
+    },
+    "kube-proxy" = {
+      version           = var.kube-proxy-version
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
 }
 
-# Associating Public Subnet in AZ2 to route table
-resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
-  subnet_id      = aws_subnet.public_subnet_az2.id
-  route_table_id = aws_route_table.public_route_table.id
+# Creating the EKS Addons
+resource "aws_eks_addon" "example" {
+  for_each = local.eks_addons
+
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = each.key
+  addon_version               = each.value.version
+  resolve_conflicts_on_update = each.value.resolve_conflicts
 }
--- variables.tf
+++ variables.tf
@@ -1,32 +1,83 @@
 # Environment
 variable "env" {
-  type = string
+  type        = string
+  description = "Environment"
 }
 
 # Type
 variable "type" {
-  type = string
+  type        = string
+  description = "Type"
 }
 
 # Stack name
 variable "cluster_name" {
-  type = string
+  type        = string
+  description = "Project Name"
 }
 
-# VPC CIDR
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+# Public subnet AZ1
+variable "public_subnet_az1_id" {
+  type        = string
+  description = "ID of Public Subnet in AZ1"
 }
 
-# CIDR of public subet in AZ1 
-variable "public_subnet_az1_cidr" {
-  type    = string
-  default = "10.0.1.0/24"
+# Public subnet AZ2
+variable "public_subnet_az2_id" {
+  type        = string
+  description = "ID of Public Subnet in AZ2"
 }
 
-# CIDR of public subet in AZ2
-variable "public_subnet_az2_cidr" {
-  type    = string
-  default = "10.0.2.0/24"
+# Security Group 
+variable "eks_security_group_id" {
+  type        = string
+  description = "ID of EKS worker node's security group"
+}
+
+# Master ARN
+variable "master_arn" {
+  type        = string
+  description = "ARN of master node"
+}
+
+# Worker ARN
+variable "worker_arn" {
+  type        = string
+  description = "ARN of worker node"
+}
+
+# Worker Node & Kubectl instance size
+variable "instance_size" {
+  type        = string
+  description = "Worker node's instance size"
+}
+
+# node count
+variable "worker_node_count" {
+  type        = string
+  description = "Worker node's count"
+}
+
+# AMI ID
+variable "image_id" {
+  type        = string
+  description = "AMI ID"
+}
+
+# Cluster Version
+variable "cluster_version" {
+  type        = string
+  description = "Cluster Version"
+}
+
+# VPC CNI Version
+variable "vpc-cni-version" {
+  type        = string
+  description = "VPC CNI Version"
+}
+
+# Kube Proxy Version
+variable "kube-proxy-version" {
+  type        = string
+  description = "Kube Proxy Version"
 }
